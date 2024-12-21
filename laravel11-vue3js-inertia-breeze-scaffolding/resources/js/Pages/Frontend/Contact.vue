34b3537b28f00b6b95a34f971c13e59bf3a90284
Shuashuaa <jovijoshuatania027@gmail.com>
2024-12-21 16:16:00 +0800

    first commit

---
+++ laravel11-vue3js-inertia-breeze-scaffolding/resources/js/Pages/Frontend/Contact.vue
@@ -0,0 +1,15 @@
+<script setup>
+import FrontendLayout from '@/Layouts/FrontendLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+</script>
+
+<template>
+    <FrontendLayout>
+
+    <Head title="Contact Page"/>
+
+    <h1>This is contact us page</h1>
+
+    </FrontendLayout>
+</template>
