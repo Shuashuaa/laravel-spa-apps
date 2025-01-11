a1cf6e83ebc36eb1ddb25736fbefc69423ea79a1
Shuashuaa <jovijoshuatania027@gmail.com>
2025-01-11 19:39:54 +0800

    011125

--- ~Laravel-Vue-Inertia-No-Breeze-Starter-Kit/app/routes/web.php
+++ ~Laravel-Vue-Inertia-No-Breeze-Starter-Kit/app/routes/web.php
@@ -5,8 +5,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'Home')->name('home');
 
-Route::inertia('/register', 'Auth/Register')->name('register');
-Route::post('/register', [AuthController::class, 'register']);
+Route::middleware('auth')->group(function(){
+    
+    Route::inertia('/dashboard', 'Dashboard')->name('dashboard');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout'); // running to function as an anchor tag and not a useForm Post
+});
 
-Route::inertia('/login', 'Auth/Login')->name('login');
-Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('guest')->group(function(){
+    
+    Route::inertia('/register', 'Auth/Register')->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
+
+    Route::inertia('/login', 'Auth/Login')->name('login'); // to the vue file
+    Route::post('/login', [AuthController::class, 'login']); // running to function as a useForm Post
+});
+
+
