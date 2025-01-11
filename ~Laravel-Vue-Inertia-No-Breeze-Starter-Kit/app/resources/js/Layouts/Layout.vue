212248e2ae65551e44b3d10f67f3c0ae83bf38a7
Shuashuaa <jovijoshuatania027@gmail.com>
2025-01-11 19:38:55 +0800

    011125

--- ~Laravel-Vue-Inertia-No-Breeze-Starter-Kit/app/resources/js/Layouts/Layout.vue
+++ ~Laravel-Vue-Inertia-No-Breeze-Starter-Kit/app/resources/js/Layouts/Layout.vue
@@ -13,13 +13,18 @@
         <header>
             <nav>
                 <div class="space-x-6">
-                    <Link :href="route('home')" class="nav-link">Home</Link>
+                    <Link :href="route('home')" class="nav-link" :class="{'bg-slate-700' : $page.component === 'Home'}">Home</Link>
                 </div>
-                <div class="space-x-6">
-                    <Link :href="route('register')" class="nav-link">Register</Link>
+                
+                <div v-if="$page.props.auth.user" class="flex"> 
+                    <Link :href="route('dashboard')" class="nav-link" :class="{'bg-slate-700' : $page.component === 'Dashboard'}">Dashboard</Link>
+                    <Link :href="route('logout')" method="post" as="button" class="nav-link">Logout</Link>
+                    <p class="nav-link">|</p>
+                    <p class="rounded-md px-3 py-2 text-sm font-medium text-white">hello!, <span class="uppercase">{{ $page.props.auth.user.name }}</span></p>
                 </div>
-                <div class="space-x-6">
-                    <Link :href="route('login')" class="nav-link">Login</Link>
+                <div v-else class="space-x-6">
+                    <Link :href="route('register')" class="nav-link" :class="{'bg-slate-700' : $page.component === 'Auth/Register'}">Register</Link>
+                    <Link :href="route('login')" class="nav-link" :class="{'bg-slate-700' : $page.component === 'Auth/Login'}">Login</Link>
                 </div>
             </nav>
         </header>
