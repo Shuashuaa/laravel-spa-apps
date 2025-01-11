b1c71858890d65a6b7953f2a0adbe30cd8d7acf2
Shuashuaa <jovijoshuatania027@gmail.com>
2025-01-11 19:45:40 +0800

    011125

--- ~Laravel-Vue-Inertia-No-Breeze-Starter-Kit/app/routes/web.php
+++ ~Laravel-Vue-Inertia-No-Breeze-Starter-Kit/app/routes/web.php
@@ -8,11 +8,12 @@ Route::inertia('/', 'Home')->name('home');
 Route::middleware('auth')->group(function(){
     
     Route::inertia('/dashboard', 'Dashboard')->name('dashboard');
+    
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout'); // running to function as an anchor tag and not a useForm Post
 });
 
 Route::middleware('guest')->group(function(){
-    
+
     Route::inertia('/register', 'Auth/Register')->name('register');
     Route::post('/register', [AuthController::class, 'register']);
 
