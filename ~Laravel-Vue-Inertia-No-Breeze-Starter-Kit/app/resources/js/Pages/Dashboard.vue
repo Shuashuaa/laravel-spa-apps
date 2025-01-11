d312b3bc8b7fe6aec1b62ca6d9c41f48e960c746
Shuashuaa <jovijoshuatania027@gmail.com>
2025-01-11 19:39:11 +0800

    011125

---
+++ ~Laravel-Vue-Inertia-No-Breeze-Starter-Kit/app/resources/js/Pages/Dashboard.vue
@@ -0,0 +1,10 @@
+<template>
+
+    <Head title="Dashboard"/>
+
+    <div>
+        <h1 class="title">
+            Welcome back {{ $page.props.auth.user.name }}
+        </h1>
+    </div>
+</template>
