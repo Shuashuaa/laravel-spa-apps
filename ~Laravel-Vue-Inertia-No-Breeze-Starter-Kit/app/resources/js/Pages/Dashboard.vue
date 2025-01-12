9a5753adc8eb677a8aafd116ff4f30481b3a93ca
Shuashuaa <jovijoshuatania027@gmail.com>
2025-01-13 00:33:56 +0800

    011325

--- ~Laravel-Vue-Inertia-No-Breeze-Starter-Kit/app/resources/js/Pages/Dashboard.vue
+++ ~Laravel-Vue-Inertia-No-Breeze-Starter-Kit/app/resources/js/Pages/Dashboard.vue
@@ -1,8 +1,10 @@
+<script setup></script>
 <template>
 
     <Head title="Dashboard"/>
 
     <div>
+        <p class="bg-green-300 text-green-900 p-2" v-if="$page.props.flash.message">{{ $page.props.flash.message }}</p>
         <h1 class="title">
             Welcome back {{ $page.props.auth.user.name }}
         </h1>
